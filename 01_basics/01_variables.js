1a4866bfd88310b663ccd463f0c8b079fab50662
PrateekDeo <154721753+PrateekDeo@users.noreply.github.com>
2023-12-24 16:40:02 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 12345
+let accountEmail = "prateekdeoaudi0532@gmail.com"
+var accountpassword =  "123564"
+accountCity = "prayagraj"
+
+console.log(accountID);
+
+//accountID = 123454568 // Not allowed
+
+accountEmail = "prateekdeoaudi999@gmail.com"
+accountpassword =  "125664864"
+accountCity = "jaigar"
+
+console.table([accountID, accountEmail, accountpassword, accountCity]);
