c599da87145cf1dae075fc4a7d79e78148ccc60b
PrateekDeo <154721753+PrateekDeo@users.noreply.github.com>
2023-12-24 17:09:03 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+// code redebility is high
+console.log("Prateek");
+
+let name = "prateek"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "prateek"); // string
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
