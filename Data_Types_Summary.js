a131d7e73a986f4385a1e6c4479ad2b581d84e7f
PrateekDeo <154721753+PrateekDeo@users.noreply.github.com>
2023-12-29 08:39:51 +0000

    Summary of Data type

---
+++ Data_Types_Summary.js
@@ -0,0 +1,33 @@
+// Primitive Data Type
+
+// 7 types :String , Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Note - JavaScript is dynamically typed language, which means that types ig variables ate derermined 
+// by the value they hold at runtime and can change throught the programe as we assign diffrent 
+// values them.
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+const id = Symbol('123')
+const anotherId = ('123')
+console.log(id === another); 
+const bignumber = 145666565554n
+
+// Refrence (Non - Primtive Type)
+
+// Array, Objects, Function
+
+const heros = ["shaltiman", "naagraj", "Doga"]
+let myObj={
+    name: Prateek,
+    age:25,
+}
+const myFunction = function(){
+   console.log("Hello world")
+}
+
+console.log(typeof bigNumber);
