eff4800f18d96448031d8f61b50357d294cdf592
PrateekDeo <154721753+PrateekDeo@users.noreply.github.com>
2023-12-29 06:33:27 +0000

    gsdfgfdsgfd

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -42,6 +42,21 @@ let str3 = " I am google AI"
 let str4 = str1 + str2 + str3
 // console.log( typeof str4);
 // console.log(str4);
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log("1" + "2"  + 3);
+// console.log((3 + 4) *(5 % 3));
+console.log(+true);
+console.log(+false);
+console.log(+"");
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
 
 
 
